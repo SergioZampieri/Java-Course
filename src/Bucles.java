490a97291a86491b24f524d935d045df54885080
sergiozampieri <gersio.zampieri@gmail.com>
2023-02-28 10:54:15 -0300

    bucles working

--- src/Bucles.java
+++ src/Bucles.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
 
 public class Bucles {
-    public static void (String[] args){
+    public static void main (String[] args){
         String clave = "Juan";
         String pass = "";
         while (clave.equals(pass)==false){
-            pass.JOptionPane.showImputDialog("Introduce la contraseña correcta");
-            if while (clave.equals(pass)==false){
+            pass=JOptionPane.showInputDialog("Introduce la contraseña correcta");
+            if (clave.equals(pass)==false){
                 System.out.print("contraseña incorrecta");
             }
 
