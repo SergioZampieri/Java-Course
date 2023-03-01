78dfe943c82ea0a8a17fa8092db39aa9f73a96cc
sergiozampieri <gersio.zampieri@gmail.com>
2023-02-28 21:23:44 -0300

    arrays 0

---
+++ src/Uso_arrays.java
@@ -0,0 +1,9 @@
+public class Uso_arrays {
+    public static void main(String[] args) {
+        int [] array = {5,23,324,234,2,76,45};
+        for (int i=0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
+
+    }
+}
