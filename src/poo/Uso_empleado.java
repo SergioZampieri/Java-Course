38e8d4a457a690ddbe0b71078f97c1952ffe7337
sergiozampieri <gersio.zampieri@gmail.com>
2023-03-02 09:34:16 -0300

    OOP getter & setters 2

---
+++ src/poo/Uso_empleado.java
@@ -0,0 +1,41 @@
+package poo;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Uso_empleado {
+    public static void main (String[] args) {
+        Empleado empleado1 = new Empleado("paco",85000,1990,12,17);
+
+        System.out.println(empleado1.getName());
+        System.out.println(empleado1.getContract());
+        empleado1.raiseSalary(12000);
+        System.out.print(empleado1.getSalary());
+
+    }
+}
+
+class Empleado{
+    public Empleado(String _name, double _salary, int _year, int _month, int _day){
+    name = _name;
+    salary = _salary;
+        GregorianCalendar calendar = new GregorianCalendar(_year,_month-1,_day);
+        contract = calendar.getTime();
+    }
+    public String getName() {
+        return name;
+    }
+    private String name;
+    public double getSalary() {
+        return salary;
+    }
+    private double salary;
+    public Date getContract() {
+        return contract;
+    }
+    public void raiseSalary(double bonus){
+        salary = salary+bonus;
+    }
+    private Date contract;
+
+}
