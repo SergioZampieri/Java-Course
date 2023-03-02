958b8eabe750eca7052dec43e68855a15a8979b7
sergiozampieri <gersio.zampieri@gmail.com>
2023-03-02 12:52:41 -0300

    static variables

--- src/poo/Pruebas.java
+++ src/poo/Pruebas.java
@@ -5,27 +5,28 @@ public class Pruebas {
         Empleados Trabajador1 = new Empleados("Carlos");
         Empleados Trabajador2 = new Empleados("Jose");
         Empleados Trabajador3 = new Empleados("Juan");
-        System.out.println(Trabajador1.getNombreSeccion());
-        System.out.println(Trabajador2.getNombreSeccion());
+        System.out.println(Trabajador1.getDatos());
+        System.out.println(Trabajador2.getDatos());
         Trabajador3.cambiaSeccion("Ventas");
-        System.out.println(Trabajador3.getNombreSeccion());
+        System.out.println(Trabajador3.getDatos());
     }
 }
 class Empleados {
     public Empleados (String _nombre){
         nombre=_nombre;
         seccion = "Administracion";
+        id=idAux;
+        idAux++;
 
     }
-
     public void cambiaSeccion (String seccion){
         this.seccion = seccion;
     }
-
-    public String getNombreSeccion() {
-        return nombre + ": "+seccion ;
+    public String getDatos() {
+        return id + "  "+ nombre + ": "+seccion ;
     }
-
+    private static int idAux=1;
+    private int id;
     private final String nombre;
     private String seccion;
 
